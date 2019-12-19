0f4accc780483a6413b89ca19e8c4783e071eb6a
Alexandre Turpin <alexandre.turpin@outlook.com>
2019-12-19 07:35:28 +0100

    Adding lib and main

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(new File("IHM/Connexion.fxml").toURI().toURL());
+        Parent root = fxmlLoader.load();
+        primaryStage.setTitle("TEST");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
