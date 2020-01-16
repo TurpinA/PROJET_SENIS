bd19506d976f49b74d17984986c5c1b57c6afa25
Yoann DUPAS <yoann.dupas@outlook.fr>
2020-01-16 10:12:58 +0100

    Adding tests TestExtractionData

---
+++ test/MODEL/TestExtractionData.java
@@ -0,0 +1,30 @@
+package MODEL;
+
+import org.junit.jupiter.api.Test;
+
+public class TestExtractionData {
+
+
+
+    @Test
+    public void rechercheAllArticleParRayon(){
+
+    }
+
+    public void rechercheRayonParResponsable(){
+
+    }
+
+    public void rechercheAllRayon(){
+
+    }
+
+    public void rechercheAllUtilisateur(){
+
+    }
+
+    public void rechercheRayonParID(){
+
+    }
+
+}
